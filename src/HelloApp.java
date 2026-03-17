4e9eb1b0ff4427d2670229aafaf2f0a27805f3f0
jitheshgokul <jitheshg111@gmail.com>
2026-03-17 09:44:24 +0530

    Completed UC4 : Added code for multiple arguments

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,8 +1,7 @@
 public class HelloApp {
     public static void main(String[] args) {
-
         if (args.length > 0) {
-            String name = args[0];
+            String name = String.join(", ", args);
             System.out.println("Hello, " + name + "!");
         } else {
             System.out.println("Hello, World!");
