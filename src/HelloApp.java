cff77c3c63a11faa62d7393a2a63c5b5c374b3bb
jitheshgokul <jitheshg111@gmail.com>
2026-03-17 09:14:02 +0530

    Completed UC2 : Added program to display 'hello' with command line argument

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -4,8 +4,7 @@ public class HelloApp {
         if (args.length > 0) {
             String name = args[0];
             System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
         }
+
     }
 }
